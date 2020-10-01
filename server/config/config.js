c13556b4a58e917dec811405da23a8483adbf2f0
Saqlainabbasi <saqlaina527@gmail.com>
2020-10-01 10:48:51 +0500

    serverCreation

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+	production: {
+		SECRET: process.env.SECRET,
+		DATABASE: process.env.MONGODB_URI
+	},
+	default: {
+		SECRET: 'supersecretpassword123',
+		DATABASE: 'mongodb://localhost:27017/bookShelf'
+	}
+};
+exports.get = function get(env) {
+	return config[env] || config.default;
+};
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+	console.log('Server Running ');
+});
